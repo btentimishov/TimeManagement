4e0a7c86f1f5f8a2b1a7a981ca39da73f2cb57a1
tentimischov <b2a2h2a2>
2018-04-20 16:22:06 +0600

    Initial commit

---
+++ app/src/main/java/baktiyar/com/testfragment/MainActivity.java
@@ -0,0 +1,19 @@
+package baktiyar.com.testfragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+    }
+}
